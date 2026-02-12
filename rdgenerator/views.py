0c14df90eba02f3748d1015cc98addfd39397071
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2026-02-12 15:53:59 -0600

    self hosted testing

--- rdgenerator/views.py
+++ rdgenerator/views.py
@@ -22,11 +22,12 @@ def generator_view(request):
     if request.method == 'POST':
         form = GenerateForm(request.POST, request.FILES)
         if form.is_valid():
-            user_secret = form.cleaned_data['sh_secret_field']
-            if _settings.SH_SECRET == user_secret:
-                selfhosted = True
-            else:
-                selfhosted = False
+            # user_secret = form.cleaned_data['sh_secret_field']
+            # if _settings.SH_SECRET == user_secret:
+            #     selfhosted = True
+            # else:
+            #     selfhosted = False
+            selfhosted = True
             platform = form.cleaned_data['platform']
             version = form.cleaned_data['version']
             delayFix = form.cleaned_data['delayFix']
